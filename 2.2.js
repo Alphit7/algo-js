4dfa851e21fa2135c1822906b1defd6e9b40d840
Alphit7 <jacobs.sylvain@gmail.com>
2023-04-06 11:31:38 +0200

    2.2

---
+++ 2.2.js
@@ -0,0 +1,10 @@
+let min = prompt("min");
+let max = prompt("max");
+let current = prompt("current");
+if ((current > min) && (current < max)){
+    alert("You got it!");
+} else if(min > max) {
+    alert("The hell is wrong with you you nobhead ?!");
+} else if ((min === max) && (min === current)){
+    alert("Nice try smartass");
+}

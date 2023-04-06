cb9fe178bacef8d3708bd1b12938e4eaea73af79
Alphit7 <jacobs.sylvain@gmail.com>
2023-04-06 12:01:43 +0200

    It now closes the window

--- 2.2.js
+++ 2.2.js
@@ -5,6 +5,7 @@ if ((current > min) && (current < max)){
     alert("You got it!");
 } else if(min > max) {
     alert("The hell is wrong with you you nobhead ?!");
+    close(window);
 } else if ((min === max) && (min === current)){
     alert("Nice try smartass");
 }

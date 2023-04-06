977513697341aa3f2ea686b73122ee6aaa8426eb
Alphit7 <jacobs.sylvain@gmail.com>
2023-04-06 11:53:01 +0200

    2.3

---
+++ 2.3.js
@@ -0,0 +1,13 @@
+/*let x = 1;
+while (x <= 100){
+    if (x%2 == 0){
+    console.log(x);
+    }
+    x++;
+}
+*/
+for (let x = 1; x <= 100; x += 1) {
+   if (x%2 === 0){
+    console.log(x);
+   } 
+}

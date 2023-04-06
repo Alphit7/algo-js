cd6debc70e8dd0bbe0ae5a7fbfdf31d71abf5932
Alphit7 <jacobs.sylvain@gmail.com>
2023-04-06 16:09:23 +0200

    4.0

---
+++ 4.0.js
@@ -0,0 +1,6 @@
+function calcSurface(a, b){
+     console.log(a * b);
+}
+let length = prompt("Longueur de votre rectangle");
+let width = prompt("La largeur de votre rectangle");
+calcSurface(length,width);

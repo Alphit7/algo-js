fd59bb4b7ce9fcc9c4a615ad6e957279b4605998
Alphit7 <jacobs.sylvain@gmail.com>
2023-04-07 09:01:32 +0200

    3.0

---
+++ 3.0.js
@@ -0,0 +1,4 @@
+let learners = ["Abel", "Alexandra","Anthony","Benjamin","Bruno","Delphine","Emilien","Ethan","Ismaël","Jodie","Julien","Louka","Loïc","Luciano","Marine","Nikko","Noa","Steve","Sylvain","Willy"]
+for (let elem of learners){
+    console.log(elem);
+}
